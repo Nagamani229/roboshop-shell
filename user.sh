d329a7fa24410aabfdad9787e1111f4d4c257cf7
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-20 08:31:39 +0530

    roboshop-shell

---
+++ user.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.devopstraining.space
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current Nodejs"  
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling Nodejs:18"  
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Installing Nodejs:18"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop 
+    VALIDATE $? "roboshop user creation"
+else
+   echo -e "roboshop user alerady exits $Y skipping $N"
+fi
+
+VALIDATE $? "Creating roboshop user"  
+
+mkdir -p /app &>> $LOGFILE
+
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip  &>> $LOGFILE
+
+VALIDATE $? "downloading user application"
+
+cd /app 
+
+unzip /tmp/user.zip &>> $LOHFILE
+
+VALIDATE $? "unzipping user"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "installing dependencies"
+
+cp /home/centos/roboshop-shell/user.service  /etc/systemd/system/user.service
+
+systemctl daemon-reload  &>> $LOGFILE
+
+VALIDATE $? "user deamon reload"
+
+systemctl enable user  &>> $LOGFILE
+
+VALIDATE $? "Enabling user"
+
+systemctl start user  &>> $LOGFILE
+
+VALIDATE $? "Starting user"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "Copying mongodb repo"
+
+dnf install mongodb-org-shell -y  &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb client"
+
+mongo --host $MONGODB_HOST </app/schema/user.js  &>> $LOGFILE
+
+VALIDATE $? "loading user data into mongodb"
