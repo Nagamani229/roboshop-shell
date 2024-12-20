9bcdac596e0e22492309022268e516375284c209
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-20 09:26:31 +0530

    roboshop-shell

---
+++ web.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.devopstraining.space
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf install nginx -y &>> $LOGFILE
+
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>> $LOGFILE
+
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>> $LOGFILE
+
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
+
+VALIDATE $? "Removed default website"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading web application"
+
+cd /usr/share/nginx/html  &>> $LOGFILE
+
+VALIDATE $? "Moving html directory"
+
+unzip /tmp/web.zip  &>> $LOGFILE
+
+VALIDATE $? " Unzipping web"
+
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+
+VALIDATE $? "Copying roboshop reverse proxy config"
+
+systemctl restart nginx &>> $LGOFILE
+
+VALIDATE $? "Restarting nginx"
+
