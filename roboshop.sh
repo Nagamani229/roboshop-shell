e660a2d123ae3fa7349afbc21359e2764b6af84a
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 22:06:16 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-AMI="ami-0b4f379183e5706b9"
-SG_ID="sg-0cd7597a2d7a778cd"
-INSTANCES=("mongodb" "redis" "rabbitmq" "mysql" "shipping" "cart" "user" "catalogue" "payment" "dispatch" "web")
+AMI=ami-0b4f379183e5706b9 #this keeps on changing
+SG_ID=sg-0cd7597a2d7a778cd #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
 
 for i in "${INSTANCES[@]}"
-do 
-echo "instance is: $i"
-  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-  then
-     INSTANCE_TYPE="t3.small"
-  else
-    INSTANCE_TYPE="t2.micro"
-  fi
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
 
-  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0cd7597a2d7a778cd --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$i}]"
-
-done
+    aws ec2 run-instances --image-id  --ami-0b4f379183e5706b9 instance-type $INSTANCE_TYPE --security-group-ids sg-0cd7597a2d7a778cd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
