392979418d1c6f59e6d5dd2f9bf32a4de901573c
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 21:35:16 +0530

    roboshop-shell

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI=""
+SG-ID=""
+INSTANCES=("mongodb" "redis" "rabbitmq" "mysql" "shipping" "cart" "user" "catalogue" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do 
+echo "instance is: $i"
+  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+  then
+     INSTANCE_TYPE="t3.small"
+  else
+    INSTANCE_TYPE="t2.micro"
+  fi
+
+  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0cd7597a2d7a778cd
+
+done
