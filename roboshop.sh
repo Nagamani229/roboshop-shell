ec53e793fce9493b4af04719025b74567a769ace
Potu Nagamani <Nagamani.X.Potu@kp.org>
2024-12-21 22:26:21 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -14,5 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0cd7597a2d7a778cd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0cd7597a2d7a778cd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'instances[0].PrivateIPAddress' --ouput text)
+    echo "$i: $IP_ADDRESS"
 done
